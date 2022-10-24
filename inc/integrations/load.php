db4ffa7a19145cfa2ecd04ddb7da1dd947da9441
Tim <github@timgreen.ws>
2022-10-24 14:11:39 +1100

    added connection screen for page load

---
+++ inc/integrations/load.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * GroundCTRL Framework
+ * Integration Connecting Screen
+ */
+
+?>
+
+<div id="editor" class="terminal shell">
+    <code data-term-input>Connecting to <?php echo $domain;?>....</code>
+    <code data-term-input>Connected</code>
+    <code data-term-input>Page load complete</code>
+</div>
