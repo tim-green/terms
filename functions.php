ff82c4898c78fc6a2efcdb409cdb0cf6c1e739f6
Tim <github@timgreen.ws>
2022-10-24 14:27:58 +1100

    commented out framework requires

--- functions.php
+++ functions.php
@@ -14,24 +14,24 @@ require_once get_template_directory() . '/inc/setup.php';
 require_once get_template_directory() . '/inc/enqueues.php'; 
 
 /* Register navmenus */
-require_once get_template_directory() . '/inc/navmenus.php'; 
+// require_once get_template_directory() . '/inc/navmenus.php'; 
 
 /* Register sidebars */
-require_once get_template_directory() . '/inc/sidebars.php'; 
+// require_once get_template_directory() . '/inc/sidebars.php'; 
 
 /* Nav Walker */
-require_once get_template_directory() . '/inc/lib/class-wp-bootstrap-navwalker.php'; 
+// require_once get_template_directory() . '/inc/lib/class-wp-bootstrap-navwalker.php'; 
 
 /* Register Custom Post Types & Taxonomies */
-foreach ( glob( get_template_directory() . '/inc/cpt/*.php' ) as $cpt ) {
-	require_once $cpt;
-}; 
+// foreach ( glob( get_template_directory() . '/inc/cpt/*.php' ) as $cpt ) {
+// 	require_once $cpt;
+// }; 
 
 /* Various global functions */
 require_once get_template_directory() . '/inc/global.php'; 
 
 /* ACF integration */
-require_once get_template_directory() . '/inc/integrations/acf.php'; 
+// require_once get_template_directory() . '/inc/integrations/acf.php'; 
 
 /* Contact Form 7 integration */
 // require_once get_template_directory() . '/inc/integrations/cf7.php'; 
@@ -40,9 +40,9 @@ require_once get_template_directory() . '/inc/integrations/acf.php';
 // require_once get_template_directory() . '/inc/searchfilter.php'; 
 
 /* Cleanup */
-require_once get_template_directory() . '/inc/cleanup.php'; 
+// require_once get_template_directory() . '/inc/cleanup.php'; 
 
 /* Custom user functions */
-require_once get_template_directory() . '/inc/custom.php'; 
+// require_once get_template_directory() . '/inc/custom.php'; 
 
 
